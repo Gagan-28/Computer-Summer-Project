d21a97b91911d063c4421575e34ed166531daa8f
Gagan-28 <84262680+Gagan-28@users.noreply.github.com>
2024-07-14 23:02:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function visit1()
+{
+    window.location = "https://www.tourmyindia.com/states/assam/";
+}
+
+function visit2()
+{
+    window.location = "https://www.tourmyindia.com/states/arunachalpradesh/";
+}
+
+function visit3()
+{
+    window.location = "https://www.tourmyindia.com/states/manipur/";
+}
+
+function visit4()
+{
+    window.location = "https://www.tourmyindia.com/states/meghalaya/";
+}
+
+function visit5()
+{
+    window.location = "https://www.tourmyindia.com/states/mizoram/";
+}
+
+function visit6()
+{
+    window.location = "https://www.tourmyindia.com/states/nagaland/";
+}
+
+function visit7()
+{
+    window.location = "https://www.tourmyindia.com/states/sikkim/";
+}
+
+function visit8()
+{
+    window.location = "https://www.tourmyindia.com/states/tripura/";
+}
